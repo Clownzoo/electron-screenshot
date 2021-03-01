1550825eb7e0159cd6b5a6dac7f757284329a1a7
[lizhipeng] <[lizhipeng@finogeeks.com]>
2021-03-01 22:26:40 +0800

    feat: add project

---
+++ src/renderer/views/home/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>解析vue文件</div>
+    <p>{{ name }}</p>
+  </div>
+</template>
+<script>
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+  setup() {
+    const name = ref('txm');
+    return {
+      name,
+    };
+  },
+});
+</script>
